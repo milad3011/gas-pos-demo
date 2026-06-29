d8303f083ba6ad3726b5f4d70a8e466994cc7af4
Milad <milad.antabil2@gmail.com>
2026-06-29 14:03:25 +0300

    Changed title

--- app/page.tsx
+++ app/page.tsx
@@ -71,8 +71,7 @@ export default function Page() {
     <main className="page" dir="rtl">
       <section className="top no-print">
         <div>
-          <h1> Gas POS Demo</h1>
-          <p>ديمو POS للتابلت / الآيفون</p>
+          <h1> نظام بيع - تجريبي </h1>
         </div>
 
         <button onClick={clear} className="danger">
